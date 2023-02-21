4492b6d2623bb3a88618efc1e98dbe202ee402da
Anteneh <anteneh.siltil@gmail.com>
2023-02-21 13:44:21 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers and returns the sum
+ * @a: first int number
+ * @b: second int number
+ * Return:0
+ */
+
+int add(int a, int b)
+
+{
+	return (a + b);
+}
