74fa62cbaedcf5e349006c0952141cbf94e63507
Anteneh <anteneh.siltil@gmail.com>
2023-02-23 09:43:52 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @x: first number
+ * @y: second number
+ * Return: Multiplication rsult
+ */
+
+int mul(int x, int y)
+
+{
+	return (x * y);
+}
