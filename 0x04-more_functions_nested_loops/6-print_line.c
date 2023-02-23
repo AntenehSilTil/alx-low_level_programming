905629c0fe9de387c0f92bf29f5793416c74708b
Anteneh <anteneh.siltil@gmail.com>
2023-02-23 11:51:04 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - It creates a straignt line
+ * @n: number of lines to draw
+ * Return:empty
+ */
+
+void print_line(int n);
+
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
