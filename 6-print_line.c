8d80acfc1cefd9b1ea5f352812fcea5a3addb1d2
Anteneh <anteneh.siltil@gmail.com>
2023-02-23 11:42:27 -0800

    6-print_line.c

--- 6-print_line.c
+++ 6-print_line.c
@@ -9,18 +9,11 @@
 void print_line(int n);
 
 {
-	int x;
+	int i;
 
-	if (n <= 0)
+	for (i = 0; i < n; i++)
 	{
-		_putchar('\n');
+		_putchar('_');
 	}
-	else
-	{
-		for (x = 0; x < n; x++)
-		{
-			_putchar(95);
-		}
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
