ab6833d498d89abf999ee22e676621579324d58c
Anteneh <anteneh.siltil@gmail.com>
2023-03-11 13:32:20 -0800

    library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * @puts:check
+ * @s: data
+ * Return: Always 0 (Success)
+**/
+
+void _puts(char *s)
+{
+	puts(s);
+}
