887ac7326387ffcf99253807bf5f551b71da7ff0
AntenehSilTil <anteneh.siltil@gmail.com>
2023-07-06 12:28:47 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of the bit 
+ * @a: number to search
+ * @i: index of the bit
+ * Return: value of the bit
+**/
+
+int get_bit(unsigned long int a, unsigned int i)
+{
+	int bvalue;
+
+	if (i > 63)
+		return (-1);
+	
+	bvalue = (a >> i) & 1;
+	
+	return (bvalue);
+}
